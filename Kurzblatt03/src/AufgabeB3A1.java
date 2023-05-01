166f6dac16482a5c1e019a1ac8b1b2186293394e
Hendrik <hendrik.leucht@dokom.net>
2023-05-01 19:02:04 +0200

    Version 1.0 - Task 1 done, Task 2 progressed

--- Kurzblatt03/src/AufgabeB3A1.java
+++ Kurzblatt03/src/AufgabeB3A1.java
@@ -3,14 +3,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class AufgabeB3A1 {
-    private int subsetSize;
+    public int subsetSize;
 
-    public static int[] readStandardIn(){
+    public static void main(String[] args) {
+        try {
+            int[] array = readStandardIn();
+            int k = Integer.parseInt(args[0]);
+            AufgabeB3A1 task1 = new AufgabeB3A1(k);
+            int[][] subsets = task1.createSubsets(task1.removeDuplicates(array));
+            for (int i = 0; i < subsets.length; i++) {
+                System.out.println(Arrays.toString(subsets[i]));
+            }
+            System.out.println("There are " + subsets.length + "subsets.");
+
+        }catch(Exception e){
+
+        }
+    }
+    public AufgabeB3A1(int subsetSize){
+        this.subsetSize = subsetSize;
+    }
+    public static int[] readStandardIn() throws NumberFormatException{
         Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
-        while(in.hasNextLine()){
-            int current = Integer.parseInt(in.nextLine());
-            list.add(current);
+        try {
+            while (in.hasNextLine()) {
+                int current = Integer.parseInt(in.nextLine());
+                list.add(current);
+            }
+        }catch(Exception e){
+            throw e;
         }
         int[] arr = new int[list.size()];
         for(int i = 0; i < arr.length; i++){
@@ -18,48 +40,41 @@ public class AufgabeB3A1 {
         }
         return arr;
     }
-    public AufgabeB3A1(int subsetSize){
-        this.subsetSize = subsetSize;
-    }
-    //TODO: createSubsets(int[] data)
     public int[][] createSubsets(int[] data){
-        if(subsetSize > data.length){
-            return null;
+        //TODO: createSubsets(int[] data) --Done--
+        if(subsetSize > data.length || subsetSize < 0){
+            return new int[0][0]; //Subset of length bigger than set does not exist
         }
         if(subsetSize == 0){
-            return new int[1][0];
+            return new int[1][0]; //Subset of length 0: Empty set
         }
-        int[][] out = new int[binom(data.length, subsetSize)][];
-        int out_pointer = 0;
-        for(int i = 0; i < data.length; i++){
-            int[] rest = new int[data.length - 1];
-            for(int j = 0; j < rest.length; j++){
-                if(i != j){
-                    rest[j] = data[j];
-                }
-            }
-            //We have the Array rest with all elements except a_i.
-
-            //Get all subsets of rest with subsetSize-1.
-            subsetSize--;
-            int[][] _new = createSubsets(rest);
-            subsetSize++;
-            for (int j = 0; j < _new.length; j++){
-                //Add data[i] to every one
-                int[] finish = new int[_new[j].length + 1];
-                for(int x = 0; x < _new[j].length; x++){
-                    finish[x] = _new[j][x];
-                }
-                finish[finish.length - 1] = data[i];
-                //Add every array to out
-                out[out_pointer] = finish;
-                out_pointer++;
-            }
+        if(subsetSize == data.length){ //Subset of length of n: Only set itself
+            int[][] out = new int[1][];
+            out[0] = data;
+            return out;
         }
+        int[] rest = Arrays.copyOfRange(data, 1, data.length); //Rest has n - 1 elements (not 1st element)
+        subsetSize--;
+        int[][] subsets1 = createSubsets(rest); //Get all subsets with (n - 1 choose k - 1)
+        //Add kth element to all subsets with k - 1 elements (add data[0] to every subset)
+        for(int i = 0; i < subsets1.length; i++){
+            int[] _new = new int[subsets1[i].length + 1];
+            for(int j = 0; j < subsets1[i].length; j++){
+                _new[j] = subsets1[i][j];
+            }
+            _new[_new.length - 1] = data[0];
+            subsets1[i] = _new;
+        }
+        subsetSize++;
+        int[][] subsets2 = createSubsets(rest); //Get all subsets with (n - 1 choose k)
+        int[][] out = merge(subsets1, subsets2);
         return out;
     }
     public int[] removeDuplicates(int[] data){
         int[] temp = data.clone();
+        if(data.length <= 1){
+            return temp;
+        }
         Arrays.sort(temp);
         int last = temp[0];
         int duplicateCounter = 0;
@@ -78,23 +93,33 @@ public class AufgabeB3A1 {
         }
         return out;
     }
-    public static void main(String[] args){
-        //TODO: main
-        int[] array = readStandardIn();
-        AufgabeB3A1 task1 = new AufgabeB3A1(Integer.parseInt(args[2]));
-        int[][] subsets = task1.createSubsets(task1.removeDuplicates(array));
-        System.out.println("All Subsets of the Array: " + Arrays.toString(array));
-        for(int i = 0; i < subsets.length; i++){
-            System.out.println(Arrays.toString(subsets[i]));
+    /**
+     * [For test purposes]: Generates an array with values between 0 and 100
+     * @param size The size of the generated array
+     * @return A randomly generated array
+     */
+    public static int[] randomArray(int size){
+        int[] out = new int[size];
+        for(int i = 0; i < size; i++){
+            out[i] = (int)(Math.random() * 100);
         }
+        return out;
     }
-    private int binom(int n, int k){
-        if(k > n || k < 0){
-            return 0;
+
+    /**
+     * Generate a new 2D-Array containing all arrays a1[i] and a2[i]
+     * @param a1 First set of arrays
+     * @param a2 Second set of arrays
+     * @return New 2D-Array containing all arrays of the input
+     */
+    private int[][] merge(int[][] a1, int[][] a2){
+        int[][] out = new int[a1.length + a2.length][];
+        for(int i = 0; i < a1.length; i++){
+            out[i] = a1[i];
         }
-        if(k == 0 || k == n){
-            return 1;
+        for(int i = 0; i < a2.length; i++){
+            out[a1.length + i] = a2[i];
         }
-        return binom(n - 1, k - 1) + binom(n - 1, k);
+        return out;
     }
 }
--- Kurzblatt03/src/AufgabeB3A2.java
+++ Kurzblatt03/src/AufgabeB3A2.java
@@ -1,54 +1,97 @@
 import java.util.Arrays;
 
 public class AufgabeB3A2 {
-    private int[] data;
-    public static void main(String[] args){
-        AufgabeB3A2 task2 = new AufgabeB3A2(AufgabeB3A1.readStandardIn());
+    public int[] data;
 
-        for(int i = 1; i < args.length; i++){
-            int k = Integer.parseInt(args[i]);
-            int[] out = task2.choosePermutation(k);
-            System.out.println("The " + k + "-th smallest permutation is:");
-            System.out.println(Arrays.toString(out));
+    public static void main(String[] args){
+        try {
+
+            int[] input = AufgabeB3A1.readStandardIn();
+            int[] k = new int[args.length];
+            for(int i = 0; i < args.length; i++){
+                k[i] = Integer.parseInt(args[i]);
+            }
+
+
+            //Input: Unsorted input with duplicates, k: Array of all k values
+            //Need to do: Sort, Remove Duplicates, Register at task2, Print
+            //int[] input = generateArray(12);
+            //int[] k = new int[]{1, 4, 17, 42, 432101234};
+
+            AufgabeB3A1 task1 = new AufgabeB3A1(-1);
+            int[] sorted = task1.removeDuplicates(input);
+            System.out.println("Sorted input: ");
+            System.out.println(Arrays.toString(sorted));
+            AufgabeB3A2 task2 = new AufgabeB3A2(sorted);
+            for(int i = 0; i < k.length; i++){
+                int[] permutation = task2.choosePermutation(k[i]);
+                System.out.println("The " + k[i] + "-smallest permutation is:");
+                System.out.println(Arrays.toString(permutation));
+            }
+        }catch(Exception e){
+
         }
     }
+
     public AufgabeB3A2(int[] data){
-        AufgabeB3A1 task1 = new AufgabeB3A1(0);
-        this.data = task1.removeDuplicates(data);
-        System.out.println("Sorted input: ");
-        Arrays.sort(this.data);
-        System.out.println(Arrays.toString(this.data));
+        this.data = data;
+    }
+    private static int[] generateArray(int size){
+        int[] out = new int[size];
+        for(int i = 0; i < out.length; i++){
+            out[i] = i + 1;
+        }
+        return out;
     }
     public int[] choosePermutation(int kSmallest){
+        //System.out.println("find(" + Arrays.toString(data) + ", k = " + kSmallest + ")");
+        if(kSmallest > fac(data.length) || kSmallest < 1 || data.length == 0){
+            System.out.println("Error: Invalid arguments!");
+            return null;
+        }
+        //Case: find([a_0], 1) => [a_0]
         if(kSmallest == 1){
             return data.clone();
         }
-        int temp = kSmallest - 1;
-        temp = (int)(temp / faculty(data.length - 1));
-        int first = data[temp];
-        int[] backup = data.clone();
+        //Case: find(a_0,...,a_{n - 1}, k) => a_i o find(a_0, ..., a_{i - 1}, a_{i + 1}, ..., a_{n - 1}, k')
+        int n = data.length;
+        int i = (int)((kSmallest - 1) / (fac(n - 1)));
+        int k_new = kSmallest - i * (int) fac(data.length - 1);
+        int first = data[i]; //Produces out-of-bounds??(i == -1 or i == 2, n = 0): Case k ==0
         //Create temporary data-array without ith element
-        int[] reduced = new int[data.length - 1];
-        for(int i = 0; i < temp; i++){
-            reduced[i] = data[i];
-        }
-        for(int i = temp + 1; i < reduced.length; i++){
-            reduced[i] = data[i];
-        }
-        this.data = reduced;
-        int[] rest = choosePermutation(1); //TODO: k' reduced to 1??
-        this.data = backup;
-        int[] out = new int[rest.length + 1];
-        for(int i = 1; i < out.length; i++){
-            out[i] = rest[i - 1];
-        }
+        int[] rest;
+        {
+            int[] backup = data.clone();
+            this.data = cut(data, i);
+            rest = choosePermutation(k_new);
+            this.data = backup;
+        } //New scope: Removes backup and reduced array from memory
+        //System.out.println("find(" + Arrays.toString(data) + ", k = " + kSmallest + "): Subarray got: " + Arrays.toString(rest));
+        //Create output: [first, <find(reduced, k')>]
+        int[] out = new int[n];
         out[0] = first;
+        for(int i3 = 1; i3 < out.length; i3++){
+            out[i3] = rest[i3 - 1];
+        }
+        //System.out.println("Output: " + Arrays.toString(out));
         return out;
     }
-    private long faculty(int x){
+    private long fac(int x){
         if(x == 0){
             return 1;
         }
-        return x * faculty(x - 1);
+        return x * fac(x - 1);
     }
+    private int[] cut(int[] arr, int x){
+        int[] out = new int[arr.length - 1];
+        for(int i = 0; i < out.length; i++){
+            if(i >= x){
+                out[i] = arr[i + 1];
+            }else{
+                out[i] = arr[i];
+            }
+        }
+        return out;
+    }
+
 }
